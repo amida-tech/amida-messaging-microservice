59b35b51952c65fabfaf747f48162a3747d97852
Nick McCally <nick@amida.com>
2018-10-22 18:00:50 -0400

    update model and add migration to correct username must be unique

---
+++ server/migrations/20181022172213-unique-email-user.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+    up(queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+      */
+        return queryInterface.changeColumn('Users','username', {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true,
+        });
+    },
+    down(queryInterface, Sequelize) {
+        return queryInterface.changeColumn('Users','username', {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: false,
+        });
+    },
+};
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -15,6 +15,7 @@ module.exports = (sequelize, DataTypes) => {
         username: {
             type: DataTypes.STRING,
             allowNull: false,
+            unique: true,
         },
     }, {
         indexes: [
