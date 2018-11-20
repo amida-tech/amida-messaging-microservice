10099854d232a06e910c3b3e742678d38ef4e57e
Grant Orndorff <grant@amida.com>
2018-11-20 14:09:58 -0500

    skipping auth requests in migration when no need

--- server/migrations/20180904132721-users_uuid.js
+++ server/migrations/20180904132721-users_uuid.js
@@ -11,79 +11,84 @@ let usersUpdated = 0;
 
 module.exports = {
     up(queryInterface, Sequelize) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-      */
+        /*
+          Add altering commands here.
+          Return a promise to correctly handle asynchronicity.
+          */
         return queryInterface.addColumn('Users', 'uuid',
             {
                 type: Sequelize.UUID,
                 unique: true,
                 after: 'id',
                 // primaryKey: true,
-            }).then(() =>
-                 // Login user that was created with token
-                   rp(
-                       {
-                           method: 'POST',
-                           url: `${config.authMicroService}/auth/login`,
-                           body: {
-                               username: `${config.microserviceAccessKey}`,
-                               password: `${config.microservicePassword}`,
-                           },
-                           json: true,
-                           headers: {
-                               'Content-Type': 'application/json',
-                           },
-                       })).then((data) => {
-                           adminToken = data.token;
+            }).then(() => User.count()).then((numUsers) => {
+                if (numUsers <= 0) {
+                    return null;
+                }
+                // Login user that was created with token
+                return rp({
+                    method: 'POST',
+                    url: `${config.authMicroService}/auth/login`,
+                    body: {
+                        username: `${config.microserviceAccessKey}`,
+                        password: `${config.microservicePassword}`,
+                    },
+                    json: true,
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                }).then((data) => {
+                    adminToken = data.token;
 
-                     // Get all users and their uuid's from auth
-                           return rp({
-                               method: 'GET',
-                               url: `${config.authMicroService}/user`,
-                               headers: {
-                                   Authorization: `Bearer ${adminToken}`,
-                                   'Content-Type': 'application/json',
-                               },
-                               json: true,
-                           });
-                       }).then((data) => {
-                     // Update all users on auth with uuids from auth, or generate one if
-                     // auth does  not contain one
+                    // Get all users and their uuid's from auth
+                    return rp({
+                        method: 'GET',
+                        url: `${config.authMicroService}/user`,
+                        headers: {
+                            Authorization: `Bearer ${adminToken}`,
+                            'Content-Type': 'application/json',
+                        },
+                        json: true,
+                    });
+                }).then((data) => {
+                    // Update all users on auth with uuids from auth, or generate one if
+                    // auth does  not contain one
 
-                           const userArray = [];
-                           data.forEach((user) => {
-                               user.username = user.username.toLowerCase();
-                               user.email = user.email.toLowerCase();
-                               userArray[user.email] = user;
-                           });
+                    const userArray = [];
+                    data.forEach((user) => {
+                        user.username = user.username.toLowerCase();
+                        user.email = user.email.toLowerCase();
+                        userArray[user.email] = user;
+                    });
 
 
-                           User.findAll({
-                               where: {
-                                   uuid: { $eq: null },
-                               },
-                           }).then((users) => {
-                      // console.log(users)
-                               if (users.length > 0) {
-                                   users.forEach((user) => {
-                                       user.username = user.username.toLowerCase();
-                                       if (!(user.username in userArray)) {
-                                           const newUUID = uuidv4();
-                                           console.log('User with email ', user.username, ' is missing from auth service');
-                                           User.update({ uuid: newUUID }, { where: { id: user.id } });
-                                           usersUpdated++;
-                                       } else {
-                                           User.update(
-                                                  { uuid: userArray[user.username].uuid },
-                                                  { where: { id: user.id } });
-                                           usersUpdated++;
-                                       }
-                                   });
-                               }
-                           });
-                       });
+                    User.findAll({
+                        where: {
+                            uuid: { $eq: null },
+                        },
+                    }).then((users) => {
+                        // console.log(users)
+                        if (users.length > 0) {
+                            users.forEach((user) => {
+                                user.username = user.username.toLowerCase();
+                                if (!(user.username in userArray)) {
+                                    const newUUID = uuidv4();
+                                    console.log('User with email ', user.username, ' is missing from auth service');
+                                    User.update({ uuid: newUUID }, { where: { id: user.id } });
+                                    usersUpdated++;
+                                } else {
+                                    User.update(
+                                        { uuid: userArray[user.username].uuid },
+                                        { where: { id: user.id } });
+                                    usersUpdated++;
+                                }
+                            });
+                        }
+                    });
+                });
+            }
+            );
+
     },
     down(queryInterface) {
         return queryInterface.removeColumn('Users', 'uuid');
